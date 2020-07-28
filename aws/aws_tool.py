02b9fe85bf067a458acf50dc2999e4f6a8284fd2
Brandon <26209299+jusbrandon@users.noreply.github.com>
2020-07-28 09:15:03 -0500

    option for aws upload_folder to upload in parallel based on specified cpu, default 1, and creation of upload_file which uploads a singuler file to s3 as suppose a directory

--- aws/aws_tool.py
+++ aws/aws_tool.py
@@ -1,34 +1,51 @@
 import boto3
 from botocore.exceptions import ClientError
 import logging
+from multiprocessing import Pool
 from data_processing import general_utilities  # From the data_processing folder, importing code to path walk directory
-
+import time
 
 """ Naming Conventions and Abbreviations 
 {
     fn: "file name"    
 }
 """
+
+
+s3 = boto3.resource('s3')  # High level variation
+s3_client = boto3.client('s3')  # Low-level variation
+
+
 class aws_tool(object):
     def __init__(self, bucket_name):
-        self.s3 = boto3.resource('s3')  # High level variation
-        self.s3_client = boto3.client('s3')  # Low-level variation
         self.bucket_name = bucket_name
+        self.root_fn = None
+        self.folder_path = None
 
     def get_bucket_object(self):
         pass
 
     # Transverse a local disk directory and upload items to S3 bucket
-    def upload_folder(self, folder_path):
-        # Turns "root\\file.txt" to "root/file.txt" for parsing
+    def upload_folder(self, folder_path, multiprocessing_cpu_count=1):
+        # Turns "root\file.txt" to "root/file.txt" for parsing
         if "\\" in folder_path:
             folder_path = folder_path.replace("\\", "/")
+        self.folder_path = folder_path
+        self.root_fn = folder_path.rsplit("/", 1)[1]
         # Grab list of objects to upload
-        files_to_upload = general_utilities.grab_all_files(folder_path)
-        root_fn = folder_path.rsplit("/", 1)[1]
-        for file_location in files_to_upload:
-            uploaded_file_name = root_fn + file_location.split(folder_path, 1)[1]
-            try:
-                response = self.s3.meta.client.upload_file(file_location, self.bucket_name, uploaded_file_name)
-            except ClientError as e:
-                logging.error(e)
+        files_to_upload = general_utilities.grab_all_files(folder_path)  # A List of files to upload
+        with Pool(multiprocessing_cpu_count) as pool:
+            pool.map(self.upload_file, files_to_upload)
+
+    # # Uploads a single file to the bucket
+    def upload_file(self, file_path):
+        if "\\" in file_path:
+            file_path = file_path.replace("\\", "/")
+        if (self.root_fn is None) or (self.folder_path is None):
+            uploaded_file_name = file_path.rsplit("/", 1)[1]
+        else:
+            uploaded_file_name = self.root_fn + file_path.split(self.folder_path, 1)[1]
+        try:
+            response = s3.meta.client.upload_file(file_path, self.bucket_name, uploaded_file_name)
+        except ClientError as e:
+            logging.error(e)
--- data_processing/general_utilities.py
+++ data_processing/general_utilities.py
@@ -1,4 +1,8 @@
 from os import walk
+import multiprocessing
+import psutil
+
+from pprint import pprint
 
 
 # Grab all files from a folder with root directory
@@ -8,4 +12,11 @@ def grab_all_files(folder_path):
         temp = [dir_path.replace("\\", "/") + "/" + files for files in file_path]
         f.extend(temp)
     return f
+
+# Grab System's Data
+def get_system_data():
+    system_data = {}
+    system_data["CPU Count"] = multiprocessing.cpu_count()/2
+    system_data["Total Physical Memory available"] = psutil.virtual_memory()
+
 
